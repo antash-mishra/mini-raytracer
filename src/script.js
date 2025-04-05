20638fbe458cc4186bc0353af18e7a946143241e
Antash <mishraantash34@gmail.com>
2025-04-05 23:40:04 +0000

    added diffused and specualr light

--- src/script.js
+++ src/script.js
@@ -1,8 +1,18 @@
 class Sphere {
-    constructor(center, radius, color) {
+    constructor(center, radius, color, specular) {
         this.center = center;
         this.radius = radius;
         this.color = color;
+        this.specular = specular;
+    }
+}
+
+class Light {
+    constructor(type, intensity, position, direction) {
+        this.type = type;
+        this.intensity = intensity;
+        this.position = position;
+        this.direction = direction;
     }
 }
 
@@ -17,7 +27,7 @@ const sizes = {
     projectionPlaneD: 1,
 }
 
-const origin = {
+const O = {
     x: sizes.originX,
     y: sizes.originY,
     z: sizes.originZ,
@@ -25,11 +35,19 @@ const origin = {
 
 const BACKGROUND_COLOR = { r: 255, g: 255, b: 255 }
 
-const sphereA = new Sphere({ x: 0, y: -1, z: 3 }, 1, { r: 255, g: 0, b: 0 })
-const sphereB = new Sphere({ x: 2, y: 0, z: 4 }, 1, { r: 0, g: 0, b: 255 })
-const sphereC = new Sphere({ x: -2, y: 0, z: 4 }, 1, { r: 0, g: 255, b: 0 })
+const sphereA = new Sphere({ x: 0, y: -1, z: 3 }, 1, { r: 255, g: 0, b: 0 }, 500)
+const sphereB = new Sphere({ x: 2, y: 0, z: 4 }, 1, { r: 0, g: 0, b: 255 }, 500)
+const sphereC = new Sphere({ x: -2, y: 0, z: 4 }, 1, { r: 0, g: 255, b: 0 }, 10)
+const sphereD = new Sphere({ x: 0, y: -5001, z: 0 }, 5000, { r: 255, g: 255, b: 0 }, 1000)
 
-const spheres = [sphereA, sphereB, sphereC]
+const spheres = [sphereA, sphereB, sphereC, sphereD]
+
+// Light 
+const ambient  = new Light("ambient", 0.2, null, null)
+const point = new Light("point", 0.6, {x: 2, y: 1, z: 0}, null)
+const directional = new Light("directional", 0.2, null, {x: 1, y: 4, z: 4})
+
+const lights = [ambient, point, directional]
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -45,6 +63,11 @@ const canvasToViewport = (a, b) => {
 
 const dot = (a, b) => {
     return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+const magnitude = (v) => {
+    const result = Math.sqrt(dot(v, v))
+    return result;
 }
 
 function intersectRaySphere(O, D, sphere) {
@@ -68,6 +91,47 @@ function intersectRaySphere(O, D, sphere) {
     return [t1, t2];
 }
 
+// Lightning Calculation
+// P => Position
+// N => Normal
+// V => Point of View by user
+// specular => specular Intensity
+const computeLighting = (P, N, V, specular) => {
+    let i = 0.0;
+    for (const light of lights) {
+        if (light.type === "ambient") {
+            i += light.intensity;
+        } else {
+            let L = { x: 0, y: 0, z: 0 };
+            if (light.type === "point") {
+                L = {
+                    x: light.position.x - P.x,
+                    y: light.position.y - P.y,
+                    z: light.position.z - P.z
+                };
+            } else {
+                L = light.direction
+            }
+            // Diffuse Light Equation
+            const n_dot_l = dot(N, L);
+            if (n_dot_l > 0) {
+                i += light.intensity * n_dot_l / (magnitude(N) * magnitude(L));
+            }
+
+            // Specular Light Eq
+            if (specular != -1) {
+                const reflection = { x: 2 * n_dot_l * N.x - L.x, y: 2 * n_dot_l * N.y - L.y, z: 2 * n_dot_l * N.z - L.z };
+                const r_dot_v = dot(reflection, V);
+                if (r_dot_v > 0) {
+                    i += light.intensity * Math.pow(r_dot_v / (magnitude(reflection) * magnitude(V)), specular);
+                }
+            }
+            
+        }
+    }
+    return i;
+}
+
 const traceRay = (O, D, t_min, t_max) => {
     let closest_t = Infinity;
     let closest_sphere = null;
@@ -89,7 +153,21 @@ const traceRay = (O, D, t_min, t_max) => {
         return BACKGROUND_COLOR;
     }
 
-    return closest_sphere.color;
+    // return closest_sphere.color;
+    // Position
+    if (closest_t === Infinity) {
+        return BACKGROUND_COLOR;
+    }
+
+    let P = {x:  O.x + closest_t * D.x, y: O.y + closest_t * D.y, z: O.z + closest_t * D.z};
+    
+    // Normal
+    let N = {x: P.x - closest_sphere.center.x, y: P.y - closest_sphere.center.y, z: P.z - closest_sphere.center.z}
+    N = {x: N.x / magnitude(N), y: N.y / magnitude(N), z: N.z / magnitude(N)}
+
+    // Lighting
+    const lighting = computeLighting(P, N, {x: -D.x, y: -D.y, z: -D.z}, closest_sphere.specular)
+    return { r: closest_sphere.color.r * lighting, g: closest_sphere.color.g * lighting, b: closest_sphere.color.b * lighting }
 }
 
 // const putPixel = (x, y, color) => {
@@ -133,7 +211,7 @@ for (let x = -sizes.width/2; x <= sizes.width/2; x++) {
     for (let y = -sizes.height/2; y <= sizes.height/2; y++) {
         // Convert from screen space to viewport space
         let D = canvasToViewport(x, y);
-        let color = traceRay(origin, D, 1, Infinity);
+        let color = traceRay(O, D, 1, Infinity);
         // Ensure proper rendering position
         putPixel(x, y, color);
     }
